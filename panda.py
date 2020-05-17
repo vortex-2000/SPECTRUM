24d3d0640eec5c6a737a1ba2426851d33c7a4ea8
vortex-2000 <53625125+vortex-2000@users.noreply.github.com>
2020-05-17 20:34:12 +0530

    Add files via upload

---
+++ panda.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+
+def convertion(cell):
+    if cell=="no":
+        return 0
+    if cell=="yes":
+        return 1
+    return cell
+
+df = pd.read_csv("student-math.csv", sep=';',
+                 converters={
+                     "schoolsup":convertion,
+                     "famsup":convertion,
+                     "paid":convertion,
+                     "activities":convertion,
+                     "nursery":convertion,
+                     "higher":convertion,
+                     "internet":convertion,
+                     "romantic":convertion
+                     })
+
+
+
+col=df["G1"]+df["G2"]+df["G3"]
+
+df["final grade"]=col
+
+del df['G1']
+del df['G2']
+del df['G3']
+
+
+
+plt.scatter(df['studytime'], df['final grade'])
+
+
+
+data=[df.studytime,df["final grade"]]
+
+fig, ax = plt.subplots()
+
+ax.boxplot(data)
+
+
+
+df.to_csv("new.csv")
+
+
